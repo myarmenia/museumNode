584f51e661d527002788862664ea12388dd4bbc1
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2024-03-01 15:53:56 +0400

    aaa

--- Controller/QrController.js
+++ Controller/QrController.js
@@ -4,7 +4,7 @@ const qrController = {
   getQr: async (req, res) => {
     try {
       const qr_generate = await qrService.getQr();
-      console.log("qr_generate =>", qr_generate);
+      console.log("qr_generate? UNIQUESLICE =>", qr_generate);
       res.status(200).send(qr_generate);
     } catch (error) {
       console.error(error);
