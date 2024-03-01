ae5c3f1ce5654af28eacdacd5b317906716242cd
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2024-03-01 15:39:38 +0400

    aaa

--- Database/Pool.js
+++ Database/Pool.js
@@ -1,25 +1,25 @@
-import mysql from "mysql2/promise";
-
+import mysql from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const connection = async () => {
-  const pool = mysql
-    .createPool({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT,
-      // ssl: true,
-    })
-    .promise();
-
   try {
+    const pool = mysql
+      .createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
+        // ssl: true,
+      })
+      .promise();
+
     return pool;
   } catch (error) {
     console.error(error);
+    throw error; // Re-throw the error to be caught by the caller
   }
 };
 
